e3d26e0bd77fa16159645f017c0599e95ef3e161
antooeda <48840207+antooeda@users.noreply.github.com>
2019-04-17 16:45:26 -0400

    vendedorNuevo

--- Taller 2/src/modelo/Vendedor.java
+++ Taller 2/src/modelo/Vendedor.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author Antonia
- */
+
 public class Vendedor {
+    private int idVendedor;
+    private String nombre;
+    private String apellido;
+    private String rut;
+
+    public Vendedor(int idVendedor, String nombre, String apellido, String rut) {
+        this.idVendedor = idVendedor;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rut = rut;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
     
+    
+   
 }
