b06a38bec6f3fad10a3e9914cbe70196a0d6c887
Estefanie Moreno <e.moreno02@ufromail.cl>
2019-04-17 16:46:52 -0400

    nada importante

--- Taller 2/src/modelo/ModeloVenta.java
+++ Taller 2/src/modelo/ModeloVenta.java
@@ -9,12 +9,14 @@ public class ModeloVenta {
     String sucursal;
     int monto;
     Date fecha;
+    Vendedor id_vendedor;
 
-    public ModeloVenta(int idVenta, String sucursal, int monto, Date fecha) {
+    public ModeloVenta(int idVenta, String sucursal, int monto, Date fecha, Vendedor id_vendedor) {
         this.idVenta = idVenta;
         this.sucursal = sucursal;
         this.monto = monto;
         this.fecha = fecha;
+        this.id_vendedor = id_vendedor;
     }
 
     public int getIdVenta() {
@@ -48,8 +50,14 @@ public class ModeloVenta {
     public void setFecha(Date fecha) {
         this.fecha = fecha;
     }
-    
-    
-    
+
+    public Vendedor getId_vendedor() {
+        return id_vendedor;
+    }
+
+    public void setId_vendedor(Vendedor id_vendedor) {
+        this.id_vendedor = id_vendedor;
+    }
+
     
 }
